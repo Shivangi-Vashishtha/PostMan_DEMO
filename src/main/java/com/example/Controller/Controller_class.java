77f8023c97b7dc16102f096a4b83e5a7f61c3e48
MY DELL <MY DELL@DESKTOP-TP7VSBJ>
2021-05-09 22:52:12 +0530

    commit

--- src/main/java/com/example/Controller/Controller_class.java
+++ src/main/java/com/example/Controller/Controller_class.java
@@ -1,5 +1,6 @@
 package com.example.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class Controller_class {
 	
+	@Value("${app.name}")//can use  @value--> app.name is define in application.properties with value
+	//@Value("${app.name:HI SHIVANGI}")	//can use --> can give value to app.name in @value itself
+	String appName;
+	
 	@GetMapping("/get")
 	public String getDetails()
 	{
-		return "Hello WORLD--SHIVANGI IS HERE";
+		//return "Hello WORLD--SHIVANGI IS HERE"; // example of simple print
+		return appName;
 	}
 
 }
